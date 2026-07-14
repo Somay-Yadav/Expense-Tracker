297cbc267ddd7d73979856e4cbb0e3114df83f5b
Somay-Yadav <somayyadav55@outlook.com>
2026-07-14 22:10:14 +0530

    JS Added

---
+++ script.js
@@ -0,0 +1,45 @@
+const expenseForm = document.getElementById("expense-form");
+const expenseName = document.getElementById("expense-name");
+const expenseAmount = document.getElementById("expense-amount");
+const expenseList = document.getElementById("expense-list");
+const totalElement = document.getElementById("total-container");
+
+let total = 0;
+
+expenseForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const name = expenseName.value.trim();
+    const amount = Number(expenseAmount.value);
+
+    if (name === "" || amount<=0) {
+        return;
+    }
+
+    // Create A List
+
+    const li = document.createElement("li")
+
+    li.innerHTML = `
+        <span>${name}</span>
+        <span>₹${amount}</span>
+        <button class="delete-btn">Delete</button>
+    `;
+
+    expenseList.appendChild(li);
+
+    // Update total
+    total += amount;
+    totalElement.textContent = total;
+
+    // Delete expense
+    li.querySelector(".delete-btn").addEventListener("click", function () {
+        total -= amount;
+        totalElement.textContent = total;
+        li.remove();
+    });
+
+    // Clear inputs
+    expenseName.value = "";
+    expenseAmount.value = "";
+});
